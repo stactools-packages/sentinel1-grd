083908a380001b8cf8ef292e8998b6e524747363
mlamare <maxim.lamare@sinergise.com>
2021-08-16 17:39:53 +0200

    Remove unnecessary code

--- src/stactools/sentinel1_grd/utils.py
+++ src/stactools/sentinel1_grd/utils.py
@@ -4,13 +4,7 @@ import pystac
 from typing import Optional, Tuple
 from pystac.extensions.eo import EOExtension
 
-
-# TODO change
-import sys
-
-sys.path.append("/home/mlamare/repos/stac/sentinel1-grd/src/stactools/sentinel1_grd")
-
-from constants import SENTINEL_POLARISATIONS
+from stactools.sentinel1_grd.constants import SENTINEL_POLARISATIONS
 
 logger = logging.getLogger(__name__)
 
@@ -56,57 +50,3 @@ def image_asset_from_href(
     else:
 
         raise ValueError(f"Unexpected asset: {asset_href}")
-
-    # # Handle auxiliary images
-
-    # if "_TCI_" in asset_href:
-    #     # True color
-    #     asset = pystac.Asset(
-    #         href=asset_href,
-    #         media_type=asset_media_type,
-    #         title="True color image",
-    #         roles=["data"],
-    #     )
-    #     asset_eo = EOExtension.ext(asset)
-    #     asset_eo.bands = [
-    #         SENTINEL_BANDS["B04"],
-    #         SENTINEL_BANDS["B03"],
-    #         SENTINEL_BANDS["B02"],
-    #     ]
-    #     set_asset_properties(asset)
-    #     return (f"visual-{asset_href[-7:-4]}", asset)
-
-    # if "_AOT_" in asset_href:
-    #     # Aerosol
-    #     asset = pystac.Asset(
-    #         href=asset_href,
-    #         media_type=asset_media_type,
-    #         title="Aerosol optical thickness (AOT)",
-    #         roles=["data"],
-    #     )
-    #     set_asset_properties(asset)
-    #     return (f"AOT-{asset_href[-7:-4]}", asset)
-
-    # if "_WVP_" in asset_href:
-    #     # Water vapor
-    #     asset = pystac.Asset(
-    #         href=asset_href,
-    #         media_type=asset_media_type,
-    #         title="Water vapour (WVP)",
-    #         roles=["data"],
-    #     )
-    #     set_asset_properties(asset)
-    #     return (f"WVP-{asset_href[-7:-4]}", asset)
-
-    # if "_SCL_" in asset_href:
-    #     # Classification map
-    #     asset = pystac.Asset(
-    #         href=asset_href,
-    #         media_type=asset_media_type,
-    #         title="Scene classfication map (SCL)",
-    #         roles=["data"],
-    #     )
-    #     set_asset_properties(asset)
-    #     return (f"SCL-{asset_href[-7:-4]}", asset)
-
-    # raise ValueError(f"Unexpected asset: {asset_href}")
